c93a0fa7bae4d4c79fdb688a42ae0f53f1d413dd
jonhcobra <joaoandrade1106@gmail.com>
2023-05-30 13:37:39 -0300

    add

---
+++ funções/q6.c
@@ -0,0 +1,14 @@
+/*Escreva um programa que receba n duplas de números inteiros e
+determine se o segundo e múltiplo do primeiro. Escreva uma função
+Multiplo que receba dois números inteiros e retorne 1 se o segundo for
+múltiplo do primeiro ou 0 em caso contrário.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void mutiplo(int *vetor,int tam){
+    
+}
+int main(){
+
+}

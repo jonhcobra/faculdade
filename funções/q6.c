872cdd61c3c067e105434b1bc9d0fc5347772ea5
jonhcobra <joaoandrade1106@gmail.com>
2023-05-30 14:40:38 -0300

    add

--- funções/q6.c
+++ funções/q6.c
@@ -6,9 +6,37 @@ múltiplo do primeiro ou 0 em caso contrário.*/
 #include <stdio.h>
 #include <stdlib.h>
 
-void mutiplo(int *vetor,int tam){
-    
+void mutiplo(int *vetor, int qtd)
+{
+    for (int i = 0; i < qtd; i += 2)
+    {
+        if (vetor[i] % vetor[i + 1] == 0)
+        {
+            printf("\n%d eh mutiplo de %d,retorno 0", vetor[i], vetor[i + 1]);
+        }
+        if (vetor[i + 1] % vetor[i] == 0)
+        {
+            printf("\n%d eh mutiplo de %d,retorno 1", vetor[i + 1], vetor[i]);
+        }
+        else
+            printf("\n%d nao tem relação de mutiplicidade %d,retorno -1", vetor[i + 1], vetor[i]);
+    }
 }
-int main(){
+int main()
+{
+    int qtd;
 
+    printf("digite a quantidade de pares:");
+    scanf("%d", &qtd);
+    qtd *= 2;
+    int *num = malloc(qtd * sizeof(int));
+    printf("\n %d \n",qtd);
+    printf("digite os numeros:\n");
+    for (int i = 0; i < qtd; i++)
+    {
+        printf("->");
+        scanf("%d", &num[i]);
+    }
+    mutiplo(num, qtd);
+    return 0;
 }

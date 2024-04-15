f823d13410a9cd2e39e8360b19a639e38582bcf8
jonhcobra <joaoandrade1106@gmail.com>
2024-04-14 22:36:24 -0300

    up

--- LP3/SimuladoProva1/Q3.java
+++ LP3/SimuladoProva1/Q3.java
@@ -3,22 +3,27 @@ package LP3.SimuladoProva1;
 public class Q3 {
     public static class Quadrilatero {
 
-        private int A[];
+        private double xa, ya;
+        private double xb, yb;
+        private double xc, yc;
+        private double xd, yd;
 
         public Quadrilatero() {
-
+            xa=0; ya=0;
+            xb=0; yb=1;
+            xc=1; yc=1;
+            xd=1; yd=0;
         }
 
-        public Quadrilatero(double xa, double ya, double xb, double yb, double xc, double yc, double xd, double yd)
-        {
+        public Quadrilatero(double xa, double ya, double xb, double yb, double xc, double yc, double xd, double yd) {
         }
 
-        public Quadrilatero(double xa, double ya, double xc, double yc)
-        {
+        public Quadrilatero(double xa, double ya, double xc, double yc) {
         }
 
         public double[] getVertices()// Retorna um array com as coordenadas dos vértices;
         {
+            return;
         }
 
         public boolean eRetangulo()// Retorna se o quadrilátero é um retângulo;
